a5aa5d56fe3445707ec07ba824dddf225599bb30
Aidan M. <aidansmcmurry@gmail.com>
2026-02-25 19:20:08 -0600

    Fixed glowing ants selection

--- Assets/Scripts/AI/CommandAnt.cs
+++ Assets/Scripts/AI/CommandAnt.cs
@@ -5,10 +5,11 @@ using UnityEngine.AI;
 public class CommandAnt : MonoBehaviour
 {
     public Camera cam;
-    public List<GameObject> selectedAnts;
+    public List<GameObject> selectedAnts = new List<GameObject>();
     public LeadNav selectedLeader;
-    public float sphereCastRadius = 2;
+    public float sphereCastRadius = 2f;
     public GameObject wayPointPrefab;
+
     private bool antSelect = false;
 
     void Awake()
@@ -18,68 +19,80 @@ public class CommandAnt : MonoBehaviour
 
     void Update()
     {
-        // if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
-        // {
-
+       
         if (Input.GetMouseButtonDown(0))
         {
             antSelect = false;
-            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
-            
-            if ((selectedLeader == null || selectedLeader.target != selectedLeader.home) && Physics.Raycast(ray, out RaycastHit hit, 500f))
-            {
-                Collider[] hits = Physics.OverlapSphere(hit.point, sphereCastRadius);
-                // Add ants
-                if(hit.transform.CompareTag("Ant") && !selectedAnts.Contains(hit.transform.gameObject))
-                {
-                    selectedAnts.Add(hit.transform.gameObject);
-                }
 
+            
+            ClearSelection();
+
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+
+            if ((selectedLeader == null || selectedLeader.target != selectedLeader.home) &&
+                Physics.Raycast(ray, out RaycastHit hit, 500f))
+            {
+               
+                if (hit.transform.CompareTag("Ant"))
+                {
+                    AddToSelection(hit.transform.gameObject);
+                }
                 else
                 {
-                    foreach(var col in hits)
+                    
+                    Collider[] hits = Physics.OverlapSphere(hit.point, sphereCastRadius);
+                    foreach (var col in hits)
                     {
-                        if(col.CompareTag("Ant") && !selectedAnts.Contains(col.transform.gameObject))
+                        if (col.CompareTag("Ant"))
                         {
-                            selectedAnts.Add(col.transform.gameObject);
+                            AddToSelection(col.transform.gameObject);
                             antSelect = true;
                         }
                     }
                 }
             }
         }
+
         
-        // -- Set a waypoint
-        if(Input.GetMouseButtonDown(1) && (selectedLeader == null || !selectedLeader.amCarryingFood))
+        if (Input.GetMouseButtonDown(1) && (selectedLeader == null || !selectedLeader.amCarryingFood))
         {
             Ray ray = cam.ScreenPointToRay(Input.mousePosition);
 
-            if(!antSelect && selectedAnts.Count != 0 && Physics.Raycast(ray, out RaycastHit hit, 500f))
+            if (!antSelect && selectedAnts.Count != 0 && Physics.Raycast(ray, out RaycastHit hit, 500f))
             {
                 Debug.Log("Selecting new leader!");
+
                 
-                foreach(GameObject ant in selectedAnts)
+                foreach (GameObject ant in selectedAnts)
                 {
                     ant.GetComponent<LeadNav>().enabled = false;
                     ant.GetComponent<FollowNav>().enabled = true;
                 }
 
+                
                 selectedAnts[0].GetComponent<FollowNav>().enabled = false;
                 selectedLeader = selectedAnts[0].GetComponent<LeadNav>();
                 selectedAnts[0].GetComponent<LeadNav>().enabled = true;
 
                 selectedLeader.home = FindFirstObjectByType<SpawnerBuilding>().transform; // TEMP
 
-                for(int i = 1; i < selectedAnts.Count; i++)
+                
+                selectedLeader.followers.Clear(); 
+                for (int i = 1; i < selectedAnts.Count; i++)
                 {
-                    selectedAnts[i].GetComponent<FollowNav>().leader = selectedLeader;
-                    selectedAnts[i].GetComponent<FollowNav>().crumbTrack = 0;
-                    selectedLeader.followers.Add(selectedAnts[i].GetComponent<FollowNav>().myAgent);
+                    var f = selectedAnts[i].GetComponent<FollowNav>();
+                    f.leader = selectedLeader;
+                    f.crumbTrack = 0;
+
+                    
+                    if (f.myAgent == null) f.myAgent = selectedAnts[i].GetComponent<NavMeshAgent>();
+                    selectedLeader.followers.Add(f.myAgent);
                 }
 
                 selectedLeader.crumbs.Clear();
+
                 
-                if(hit.transform.CompareTag("Food"))
+                if (hit.transform.CompareTag("Food"))
                     selectedLeader.target = hit.transform;
                 else
                     selectedLeader.target = Instantiate(wayPointPrefab, hit.point, Quaternion.identity).transform;
@@ -89,15 +102,55 @@ public class CommandAnt : MonoBehaviour
 
                 for (int i = 0; i < selectedLeader.followers.Count; i++)
                     selectedLeader.followers[i].isStopped = false;
-        }
-        }
 
-        else if (Input.GetKey(KeyCode.LeftShift) && Input.GetKeyDown(KeyCode.R))
+                
+                SetLeaderGlow(selectedAnts[0], true);
+            }
+        }
+        else if ((Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift)) && Input.GetKeyDown(KeyCode.R))
         {
-            selectedAnts.Clear();
-            selectedLeader = null;
+            ClearSelection();
+        }
+    }
+
+    void ClearSelection()
+    {
+       
+        foreach (var ant in selectedAnts)
+        {
+            if (ant == null) continue;
+            var glow = ant.GetComponent<AntSelectGlow>();
+            if (glow != null) glow.SetSelected(false);
         }
 
-        //  }
+        selectedAnts.Clear();
+        selectedLeader = null;
+    }
+
+    void AddToSelection(GameObject ant)
+    {
+        if (ant == null) return;
+        if (selectedAnts.Contains(ant)) return;
+
+        selectedAnts.Add(ant);
+
+        var glow = ant.GetComponent<AntSelectGlow>();
+        if (glow != null) glow.SetSelected(true);
+    }
+
+    void SetLeaderGlow(GameObject leaderAnt, bool isLeader)
+    {
+        var glow = leaderAnt.GetComponent<AntSelectGlow>();
+        if (glow == null) return;
+
+        if (isLeader)
+        {
+            
+            glow.EnableGlow(Color.yellow, 3f);
+        }
+        else
+        {
+            glow.SetSelected(true);
+        }
     }
 }
--- Assets/Scripts/AntConglomerate/AntSelectGlow.cs
+++ Assets/Scripts/AntConglomerate/AntSelectGlow.cs
@@ -2,23 +2,71 @@ using UnityEngine;
 
 public class AntSelectGlow : MonoBehaviour
 {
-    private Renderer rend;
-    private Color originalEmission;
+    [Header("Glow Settings")]
+    public Color glowColor = Color.green;
+    public float glowIntensity = 2.5f;
 
-    void Start()
+    Renderer[] renderers;
+    MaterialPropertyBlock mpb;
+    Color[][] originalEmissions;
+
+    static readonly int EmissionId = Shader.PropertyToID("_EmissionColor");
+
+    void Awake()
     {
-        rend = GetComponent<Renderer>();
-        originalEmission = rend.material.GetColor("_EmissionColor");
+        renderers = GetComponentsInChildren<Renderer>(true);
+        mpb = new MaterialPropertyBlock();
+
+        originalEmissions = new Color[renderers.Length][];
+        for (int r = 0; r < renderers.Length; r++)
+        {
+            var mats = renderers[r].sharedMaterials;
+            originalEmissions[r] = new Color[mats.Length];
+
+            for (int i = 0; i < mats.Length; i++)
+            {
+                if (mats[i] != null && mats[i].HasProperty(EmissionId))
+                    originalEmissions[r][i] = mats[i].GetColor(EmissionId);
+                else
+                    originalEmissions[r][i] = Color.black;
+            }
+        }
     }
 
-    public void EnableGlow(Color glowColor)
+    public void SetSelected(bool selected)
     {
-        rend.material.EnableKeyword("_EMISSION");
-        rend.material.SetColor("_EmissionColor", glowColor);
+        if (selected) EnableGlow(glowColor, glowIntensity);
+        else DisableGlow();
+    }
+
+    public void EnableGlow(Color color, float intensity)
+    {
+        Color hdr = color * intensity;
+
+        for (int r = 0; r < renderers.Length; r++)
+        {
+            
+            var mats = renderers[r].sharedMaterials;
+            for (int i = 0; i < mats.Length; i++)
+                if (mats[i] != null) mats[i].EnableKeyword("_EMISSION");
+
+            
+            renderers[r].GetPropertyBlock(mpb);
+            mpb.SetColor(EmissionId, hdr);
+            renderers[r].SetPropertyBlock(mpb);
+        }
     }
 
     public void DisableGlow()
     {
-        rend.material.SetColor("_EmissionColor", originalEmission);
+        for (int r = 0; r < renderers.Length; r++)
+        {
+            renderers[r].GetPropertyBlock(mpb);
+
+        
+            mpb.SetColor(EmissionId, originalEmissions[r].Length > 0 ? originalEmissions[r][0] : Color.black);
+
+            renderers[r].SetPropertyBlock(mpb);
+        }
     }
 }
