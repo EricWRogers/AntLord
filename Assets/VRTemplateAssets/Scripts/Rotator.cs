56dfe9264477d43faee4ef7c32e6521a27850289
Nathan <nathanielorozco42@gmail.com>
2026-03-03 14:08:58 -0600

    Vr Tinkering

--- Assets/VRTemplateAssets/Scripts/Rotator.cs
+++ Assets/VRTemplateAssets/Scripts/Rotator.cs
@@ -5,19 +5,27 @@ namespace Unity.VRTemplate
     /// <summary>
     /// Rotates this object at a user defined speed
     /// </summary>
+    
     public class Rotator : MonoBehaviour
     {
         [SerializeField, Tooltip("Angular velocity in degrees per second")]
         Vector3 m_Velocity;
+        bool isRotating = false;
 
         void Update()
         {
             transform.Rotate(m_Velocity * Time.deltaTime);
         }
 
-        public void StartRotation()
+        public void ChangeRotation()
         {
-            m_Velocity = new Vector3(10, 10, 10);
+            isRotating = !isRotating;
+
+            if (isRotating)
+                m_Velocity = new Vector3(10, 0, 10);
+            
+            if(!isRotating)
+                m_Velocity = new Vector3(0, 0, 0);
         }
     }
 }
