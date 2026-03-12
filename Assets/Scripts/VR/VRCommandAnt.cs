d81ffd0517732eb0bedfe9ce4f2a38407e813062
Scott Schroeder <schrosam@gmail.com>
2026-03-11 19:51:29 -0500

    The Cooler VRCommandAnt

--- Assets/Scripts/VR/VRCommandAnt.cs
+++ Assets/Scripts/VR/VRCommandAnt.cs
@@ -1,6 +1,5 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.XR.Interaction.Toolkit.Interactors;
 
 public class VRCommandAnt : MonoBehaviour
@@ -8,8 +7,9 @@ public class VRCommandAnt : MonoBehaviour
 
     public InputActionReference inputActionReference;
     public XRRayInteractor rayInteractor;
-
     private InputAction RightTriggerAction;
+
+    public float sphereCastRadius = 2.0f;
 
     void Start()
     {
@@ -33,15 +33,42 @@ public class VRCommandAnt : MonoBehaviour
             {
                 //Debug.Log("Trigger pulled"!);
 
-                RaycastHit rayHit;
+                //RaycastHit rayHit;
 
-                if (rayInteractor.TryGetCurrent3DRaycastHit(out rayHit))
-                {
-                    Debug.Log("raycast hit:" + rayHit.transform.gameObject.name);
+            //     if (rayInteractor.TryGetCurrent3DRaycastHit(out rayHit))
+            //     {
+                    
+            //         Debug.Log("raycast hit:" + rayHit.transform.gameObject.name);
 
-                    if (rayHit.transform.gameObject.CompareTag("Ant"))
+            //         if (rayHit.transform.gameObject.CompareTag("Ant"))
+            //         {
+            //             Debug.Log("I spy a little ant!");
+            //         }
+            //     }
+            // }
+
+                if(rayInteractor.TryGetCurrent3DRaycastHit(out RaycastHit hit)){
+
+                    Collider[] hits = Physics.OverlapSphere(hit.point, sphereCastRadius);
+
+                    // Direct click
+                    if (hit.transform.CompareTag("Ant") && hit.transform.GetComponent<AntBrain>().antType.teamID == 0)
                     {
                         Debug.Log("I spy a little ant!");
+                    }
+                    // Secondary "close enough" selection
+                    else
+                    {
+                        
+                        foreach (Collider col in hits)
+                        {
+                            if (col.CompareTag("Ant") && col.transform.GetComponent<AntBrain>().antType.teamID == 0)
+                            {
+                                Debug.Log("I spy a little ant!");
+                                // stop looking after we find the first ant
+                                break;
+                            }
+                        }
                     }
                 }
             }
