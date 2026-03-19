844f6a1f3ff7a4e30bd2cc8d6b7cbf65fafdb279
CooperCantrell <coopercantrell56@gmail.com>
2026-03-19 12:05:26 -0500

    enemy ant spawner now spawns randomly

--- Assets/Scripts/BuildingScripts/EnemyBuilding.cs
+++ Assets/Scripts/BuildingScripts/EnemyBuilding.cs
@@ -7,7 +7,8 @@ public class EnemyBuilding : Buildings
     public Transform spawnPoint;
     public float spawnPadding = 5.0f;
     float timer = 0.0f;
-    public float spawnCooldown = 8.0f;
+    public float spawnCooldown = 10.0f;
+    public float radius = 5.0f;
     public int maxAnts = 10;
     List<GameObject> ants = new List<GameObject>();
     public GameObject antPrefab;
@@ -38,7 +39,20 @@ public class EnemyBuilding : Buildings
 
     void SpawnAnt()
     {
-        Instantiate(antPrefab, spawnPoint.position, Quaternion.identity); //idk what to do about rotation at the moment so...
+        float randomX = 0.0f;
+        float randomZ = 0.0f;
+        while (randomX < transform.localScale.x && randomX > -transform.localScale.x)
+        {
+            randomX = transform.position.x + transform.localScale.x + Random.Range(-(transform.localScale.x + radius), transform.localScale.x + radius);
+        }
+        while (randomZ < transform.localScale.z && randomZ > -transform.localScale.z)
+        {
+            randomZ = transform.position.z + transform.localScale.z + Random.Range(-(transform.localScale.z + radius), transform.localScale.z + radius);
+        }
+
+        Vector3 spawn = new Vector3(randomX, transform.position.y, randomZ);
+
+        Instantiate(antPrefab, spawn, Quaternion.identity); //idk what to do about rotation at the moment so...
         
     }
 }
