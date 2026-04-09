d92334ae15b687a5e71b01e53d6fddfd879125f1
CooperCantrell <coopercantrell56@gmail.com>
2026-04-09 12:04:06 -0500

    start of turret

---
+++ Assets/Scripts/BuildingScripts/Turret.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+
+    public Transform target;
+    public float range = 10f;
+    public Transform partToRotate;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        InvokeRepeating("UpdateTarget", 0f, 0.5f);
+    }
+
+    void UpdateTarget()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Ant");
+        float shortestDistance = Mathf.Infinity;
+        GameObject nearestEnemy = null;
+
+        foreach(GameObject enemy in enemies)
+        {
+            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+
+            if(distanceToEnemy < shortestDistance){
+                shortestDistance = distanceToEnemy;
+                nearestEnemy = enemy;
+            }
+        }
+
+        if(nearestEnemy != null && shortestDistance <= range){
+            target = nearestEnemy.transform;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(target == null){
+            return;
+        }
+
+        Vector3 dir = target.position - transform.position;
+        Quaternion lookRotation = Quaternion.LookRotation(dir);
+        Vector3 rotation = lookRotation.eulerAngles;
+        
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(transform.position,range);
+    }
+}
