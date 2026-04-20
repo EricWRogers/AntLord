34b240b14292a6c373a7251b2fceec87f3c93954
Nathan <nathanielorozco42@gmail.com>
2026-04-20 13:06:09 -0500

    Moved UI Assets Folder into Art Assets Folder

--- Assets/Scripts/FlyCam and VrControls/LevelSelectCode.cs
+++ Assets/Scripts/FlyCam and VrControls/LevelSelectCode.cs
@@ -3,11 +3,55 @@ using UnityEngine.SceneManagement;
 
 public class LevelSelectCode : MonoBehaviour
 {
+    //Create Singleton
+    public static LevelSelectCode instance = null;
+
+    //Set Playmode
+    public bool VRIsActivated;
+
+    public GameObject DesktopCam;
+    public GameObject VRCam;
+
+    //LevelSelect
     public GameObject levelSelectMenu;
 
+    void Awake()
+    {
+        if (instance == null)
+        {
+            Debug.Log("Singleton init");
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+    }
     void Start()
     {
+        SetPlayMode();
         levelSelectMenu.SetActive(false);
+    }
+
+    public void SetPlayMode()
+    {
+        if (VRIsActivated){
+            DesktopCam.SetActive(false);
+            VRCam.SetActive(true);
+        }
+        else
+        {
+            DesktopCam.SetActive(true);
+            VRCam.SetActive(false);
+        }
+    }
+
+    public void StartLevel(string level)
+    {
+        SceneManager.LoadScene(level);
+        Debug.Log("Game Started: " + level);
     }
 
      public void StartLevel1(string level){
