2a74273d65c4cab568d4bd092e2839da5426bd4f
Nathan <nathanielorozco42@gmail.com>
2026-04-23 12:22:03 -0500

    New Sprites Added to MM

--- Assets/Scripts/AntConglomerate/Cristian/StatsText.cs
+++ Assets/Scripts/AntConglomerate/Cristian/StatsText.cs
@@ -10,6 +10,7 @@ public class StatsText : MonoBehaviour
     public TextMeshProUGUI antCountText;
 
     private SpawnFoodBites _spawnFoodBites;
+    private ResourceManager _rM;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -21,17 +22,18 @@ public class StatsText : MonoBehaviour
     void Update()
     {
         //count all collected food
-
+        foodText.text = "Food: " + _rM.food.ToString();
 
         //count all collected sticks
-
+        stonesText.text = "Sticks: " + _rM.rocks.ToString();
 
         //count all collected stones
+        sticksText.text = "Stones: " + _rM.sticks.ToString();
 
 
         //count all of the players ants
         AntSelectGlow[] ants = FindObjectsByType<AntSelectGlow>(FindObjectsSortMode.None);
-        antCountText.text = ("Total Ants: " + ants.Length.ToString());
+        antCountText.text = "Total Ants: " + ants.Length.ToString();
         if (ants.Length == 0)
         {
             antCountText.color = new Color(212, 0, 20, 255);
